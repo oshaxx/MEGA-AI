253dce446864bec401a7490347990cd838615513
OshaX <87685400+oshaxx@users.noreply.github.com>
2025-03-14 10:58:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '94782722212' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -19,8 +19,8 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
+global.botname = process.env.BOTNAME || 'Osha X Bot';
+global.pairingNumber = process.env.BOT_NUMBER || '94767617500' ;  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
@@ -42,7 +42,7 @@ global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 
-global.canal = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.canal = 'https://whatsapp.com/channel/0029Vb4MnKKIXnlzKRHbON3i'
 
 
 global.APIs = {
@@ -73,16 +73,16 @@ global.APIKeys = {
 
 // Sticker WM
 global.premium = 'true'
-global.packname = 'MEGA-AI'
-global.author = 'GlobalTechInfo'
-global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
-global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/global.techinfo'
-global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
-global.fgsc = 'https://github.com/GlobalTechInfo/MEGA-AI'
-global.fgyt = 'https://youtube.com/@GlobalTechInfo'
-global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
-global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
-global.thumb = fs.readFileSync('./assets/A.jpg')
+global.packname = 'Osha X Bot'
+global.author = 'Osha Xz'
+global.menuvid = 'https://i.imgur.com/RuVjAmx.mp4'
+global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/_osha_z_/'
+global.dygp = 'https://whatsapp.com/channel/0029Vb4MnKKIXnlzKRHbON3i'
+global.fgsc = ''
+global.fgyt = ''
+global.fgpyp = ''
+global.fglog = 'https://raw.githubusercontent.com/oshaxx/imag/refs/heads/main/file.jpg'
+global.thumb = 'https://raw.githubusercontent.com/oshaxx/imag/refs/heads/main/file.jpg'
 
 global.wait = '⏳'
 global.rwait = '⏳'
